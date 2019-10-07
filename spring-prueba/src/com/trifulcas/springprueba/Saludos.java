9a4dee9f37f2f2e062e04f4aaed2731dffb77a54
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2019-10-07 16:02:56 +0200

    Spring prueba

---
+++ spring-prueba/src/com/trifulcas/springprueba/Saludos.java
@@ -0,0 +1,12 @@
+package com.trifulcas.springprueba;
+
+public class Saludos implements ISaludo {
+
+	public String hola() {
+		return "Hola que tal";
+	}
+	public String adios() {
+		return "Adios que te vaya bien";
+	}
+	
+}
---
+++ spring-prueba/src/com/trifulcas/springprueba/Vecino.java
@@ -0,0 +1,17 @@
+package com.trifulcas.springprueba;
+
+public class Vecino implements ISaludo {
+
+	@Override
+	public String hola() {
+		// TODO Auto-generated method stub
+		return "Holi vecinito";
+	}
+
+	@Override
+	public String adios() {
+		// TODO Auto-generated method stub
+		return "Hasta luegi!!";
+	}
+
+}
