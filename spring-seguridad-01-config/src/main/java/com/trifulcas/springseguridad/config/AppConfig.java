2e98b00e4b6f61ae4e99cce5d41621add64113cf
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2019-11-05 18:52:09 +0100

    config

---
+++ spring-seguridad-01-config/src/main/java/com/trifulcas/springseguridad/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.trifulcas.springseguridad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.trifulcas.springseguridad")
+public class AppConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+
+}
